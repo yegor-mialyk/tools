b3abb3096295471d52e6e2ef3632f851c08b159a
Yegor Mialyk <16239726+yegor-mialyk@users.noreply.github.com>
2020-07-13 22:10:08 -0700

    Cleanup build scripts

--- Sudo/build.cmd
+++ Sudo/build.cmd
@@ -20,11 +20,3 @@ if %errorlevel% neq 0 exit /b 1
 
 %DCC% Sud.pas -NSSystem;Winapi;Vcl -q -u..\..\Projects\Lib
 if %errorlevel% neq 0 exit /b 1
-
-rem exit
-
-if not exist ..\releases\Sudo\x86 md ..\releases\Sudo\x86
-if not exist ..\releases\Sudo\x64 md ..\releases\Sudo\x64
-
-if [%DCC%] == [dcc32] copy Su.exe ..\releases\Sudo\x86 & copy Sud.dll ..\releases\Sudo\x86
-if [%DCC%] == [dcc64] copy Su.exe ..\releases\Sudo\x64 & copy Sud.dll ..\releases\Sudo\x64
